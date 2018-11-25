b6b656e3ebea5fc845effda07d0c5a2b031ea8f8
MariaSobol <sablemari@gmail.com>
2018-11-25 15:59:43 +0300

    set new state object "mail1"

--- Mail/main.cpp
+++ Mail/main.cpp
@@ -6,6 +6,7 @@ int main()
 	Mail mail1("Отправление N1");
 	mail1.setState(new Shipped);
 	mail1.setState(new ArrivedForSorting);
+	mail1.setState(new ShippedFromSorting);
 	mail1.setState(new Lost);
 
 	system("pause");
